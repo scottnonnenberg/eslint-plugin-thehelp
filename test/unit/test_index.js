425b0cbf9afecbe01c65a740033bddea4fcc43e2
Scott Nonnenberg <scott@nonnenberg.com>
2021-11-15 21:35:17 -0800

    fix(test): Fix rule existence test

--- test/unit/test_index.js
+++ test/unit/test_index.js
@@ -8,14 +8,14 @@ var index = require('src/index');
 
 describe('index', function() {
   it('has truthy property "absolute-or-current-dir"', function() {
-    expect(index).to.have.deep.property('rules.absolute-or-current-dir').that.exist;
+    expect(index.rules).to.have.property('absolute-or-current-dir').that.exist;
   });
 
   it('has truthy property "no-mutation"', function() {
-    expect(index).to.have.deep.property('rules.no-mutation').that.exist;
+    expect(index.rules).to.have.property('no-mutation').that.exist;
   });
 
   it('has truthy property "no-array-mutation"', function() {
-    expect(index).to.have.deep.property('rules.no-array-mutation').that.exist;
+    expect(index.rules).to.have.property('no-array-mutation').that.exist;
   });
 });
